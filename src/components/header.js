dfb468848e1ec63feb5a4db6e31baad322a7afea
Michał Kuliś <michalkulis1993@gmail.com>
2019-10-23 23:52:27 +0200

    Add basic changes for Gatsby Project

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,22 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from 'styled-components'
+
+const HeaderApp = styled.header`
+  background-color: blue;
+`;
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <HeaderApp>
+    <div>
+      <h1>
+        <Link to="/">
           {siteTitle}
         </Link>
       </h1>
     </div>
-  </header>
+  </HeaderApp>
 )
 
 Header.propTypes = {
